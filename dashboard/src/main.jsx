d9e7aefa1e7a023ca0346c1466478ca7754cb598
MdAkashSheikh <nmakashsheikh@gmail.com>
2024-01-27 01:48:24 +0600

    Register and Login Page Add

--- dashboard/src/main.jsx
+++ dashboard/src/main.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React, { lazy, Suspense } from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
+const App = lazy(() => import('./App'));
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-      <App />
+      <Suspense fallback='loading...'>
+          <App />
+      </Suspense>
   </BrowserRouter>
 )
